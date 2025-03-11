958c873af4d80a999fd71c9681875bea0b1dd232
mouravocal <diego.moura3@gmail.com>
2025-03-11 15:53:58 -0300

    fix: :bug: fix icon svg name

--- vite.config.ts
+++ vite.config.ts
@@ -24,13 +24,13 @@ export default defineConfig(() => ({
         theme_color: "#2B6CB0",
         icons: [
           {
-            src: "/vite.svg",
+            src: "/bunny.svg",
             sizes: "192x192",
             type: "image/svg+xml",
             purpose: "any maskable"
           },
           {
-            src: "/vite.svg",
+            src: "/bunny.svg",
             sizes: "512x512",
             type: "image/svg+xml",
             purpose: "any maskable"
